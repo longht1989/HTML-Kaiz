53c3ba2605655d5ba7eb0f7309e36c717230339e
longht1989 <longht@epi.com.vn>
2017-05-30 16:33:41 +0700

    Done index Kaiz

--- source/js/custom.js
+++ source/js/custom.js
@@ -9,4 +9,22 @@ $(function() {
         $(".mobile__menu").slideToggle('fast');
     });
     $('.story-listing .story--rare').matchHeight();
+    $('.story--img-shadow').each(function() {
+        var imgshadow = $(this).find('.img-responsive').attr("src");
+        $(this).append($('<div class="img-shadow"></div>'));
+        $(this).find('.img-shadow').css("background-image", "url(" + imgshadow + ")");
+    });
+    $(".featured-slider").bxSlider({
+        nextText: '',
+        prevText: '',
+        nextSelector: '.ion-android-arrow-forward',
+        prevSelector: '.ion-android-arrow-back',
+        pager: 0
+    });
+    $(".quote-listing").bxSlider({
+        nextText: '',
+        prevText: '',
+        nextSelector: '',
+        prevSelector: ''
+    });
 });
